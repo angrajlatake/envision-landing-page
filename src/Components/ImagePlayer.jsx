da132f3b0be93a9ff7455cc437dea0ff7969b38b
angrajlatake <angraj.latake@gmail.com>
2023-02-12 16:19:58 -0500

    git lazypush testing was successful

--- src/Components/ImagePlayer.jsx
+++ src/Components/ImagePlayer.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 const ImagePlayer = ({ src }) => {
-  //this is  git custom command test
-
   return (
     <motion.div
       className="img-player"
